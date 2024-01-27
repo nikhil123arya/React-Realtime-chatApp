e4f82a132ea8b6979e32c2c182668938802cc7f5
Nikhil Arya <nikhilakabob@gmail.com>
2024-01-27 22:51:18 +0530

    Protected Routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./components/PrivateRoutes";
 import Room from "./pages/Room";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <>
-      <Room />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+
+          <Route element={<PrivateRoutes/>}>
+            <Route path="/" element={<Room/>} />
+          </Route>
+
+        <Route path="/" element={<Room/>}/>
+      </Routes>
+
+    </Router>
   );
 }
 
-export default App;
+export default App; 
